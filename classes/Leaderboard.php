57d02f57e3c6d36ff97783fbb250528bfef5527b
NeKz <nekzor@users.noreply.github.com>
2023-05-17 06:21:00 +0200

    Skip pending if there are no requirements

--- classes/Leaderboard.php
+++ classes/Leaderboard.php
@@ -1355,6 +1355,8 @@ class Leaderboard
         $result = self::getChange($changeLogId);
         // Getting requirements (Old/inactive as well)
         $requirements = self::getEvidenceRequirments(false);
+        if (!count($requirements)) return 0;
+
         $dateTime = $result['time_gained'];
 
         // if video or demo required
