ed4451ce768591ec2dda53d82a0987517468286a
NeKz <nekzor@users.noreply.github.com>
2023-05-21 11:21:33 +0200

    Replace secret files with a single config file

---
+++ classes/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Provides values from the `.config.json` file.
+ * 
+ * Example: `Config::get()->database_host`
+ */
+final class Config {
+    public readonly string $database_host;
+    public readonly string $database_user;
+    public readonly string $database_pass;
+    public readonly string $database_name;
+    public readonly string $discord_webhook_id;
+    public readonly string $discord_webhook_token;
+    public readonly string $discord_webhook_mdp;
+    public readonly string $steam_api_key;
+
+    private static $_instance;
+
+    public static function get(): Config {
+        return self::$_instance ??= new Config();
+    }
+
+    private function __construct() {
+        foreach (json_decode(file_get_contents(ROOT_PATH . '/.config.json'), true) as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = strval($value);
+            }
+        }
+    }
+}
