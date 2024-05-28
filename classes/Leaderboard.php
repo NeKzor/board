ea870e26a99e633464bf3e6419aad14d6defb482
NeKz <nekzor@users.noreply.github.com>
2024-05-28 08:10:59 +0200

    Fix query syntax error

--- classes/Leaderboard.php
+++ classes/Leaderboard.php
@@ -1713,7 +1713,7 @@ class Leaderboard
 
         $data = Database::unsafe_raw(
             "SELECT id
-                  , rank
+                  , evidence_requirements.rank
                   , demo
                   , video
                   , active
