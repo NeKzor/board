p2sr/Portal2Boards

Add regression tests

tests/auth_test.ts
@@ -1,0 +1,313 @@
+import { assert, assertEquals } from "jsr:@std/assert";
+
+const API = "https://board.portal2.local";
+const PROFILE = "76561198049848090";
+const COOKIE = "PHPSESSID=rfaktvssd448o1t93nvnhh187t";
+
+Deno.test("Manual submission", async (t) => {
+  let id = "";
+
+  await t.step("Submit change", async () => {
+    const body = new FormData();
+    body.append("profileNumber", PROFILE);
+    body.append("chamber", "47458");
+    body.append("score", "2300");
+    body.append("youtubeID", "DM3a55hXiI0");
+    body.append(
+      "demoFile",
+      new Blob([await Deno.readFile("./tests/data/short.dem")]),
+    );
+    body.append("comment", "test");
+
+    const res = await fetch(`${API}/submitChange`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    const run = await res.json();
+    assert(typeof run === "object");
+
+    assertEquals(run.player_name, "NeKz");
+    assertEquals(
+      run.avatar,
+      "https://avatars.steamstatic.com/9a86e6554aee395b3ac37d96a808335363eb79ff_full.jpg",
+    );
+    assertEquals(run.profile_number, "76561198049848090");
+    assertEquals(run.score, 2300);
+    assert(run.id);
+    assert(run.pre_rank);
+    assertEquals(run.post_rank, 5);
+    assertEquals(run.wr_gain, 0);
+    assert(run.time_gained);
+    assertEquals(run.hasDemo, 1);
+    assertEquals(run.youtubeID, "DM3a55hXiI0");
+    assertEquals(run.note, "test");
+    assertEquals(run.banned, 0);
+    assertEquals(run.submission, 1);
+    assertEquals(run.pending, 0);
+    assertEquals(run.autorender_id, null);
+    assert(run.previous_score);
+    assertEquals(run.chamberName, "Portal Gun");
+    assertEquals(run.chapterId, 7);
+    assertEquals(run.mapid, "47458");
+    assert(run.improvement >= 0);
+    assert(run.rank_improvement >= 0);
+    assertEquals(run.pre_points, null);
+    assertEquals(run.post_point, null);
+    assertEquals(run.point_improvement, null);
+
+    id = run.id.toString();
+  });
+
+  await t.step("Delete comment", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteComment`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Set comment", async () => {
+    const body = new FormData();
+    body.append("id", id);
+    body.append("comment", "test 123");
+
+    const res = await fetch(`${API}/setComment`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Delete YouTube ID", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteYoutubeID`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Set YouTube ID", async () => {
+    const body = new FormData();
+    body.append("id", id);
+    body.append("youtubeID", "DM3a55hXiI0");
+
+    const res = await fetch(`${API}/setYoutubeID`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Delete demo", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteDemo`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Upload demo", async () => {
+    const body = new FormData();
+    body.append("id", id);
+    body.append(
+      "demoFile",
+      new Blob([await Deno.readFile("./tests/data/short.dem")]),
+    );
+
+    const res = await fetch(`${API}/uploadDemo`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Set score ban status", async () => {
+    const body = new FormData();
+    body.append("id", "82802");
+    body.append("banStatus", "1");
+
+    const res = await fetch(`${API}/setScoreBanStatus`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Verify score", async () => {
+    const body = new FormData();
+    body.append("id", "82802");
+
+    const res = await fetch(`${API}/verifyScore`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Delete submission", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteSubmission`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+});
+
+Deno.test("Set profile ban status", async (t) => {
+  await t.step("Ban", async () => {
+    const body = new FormData();
+    body.append("profileNumber", PROFILE);
+    body.append("banStatus", "1");
+
+    const res = await fetch(`${API}/setProfileBanStatus`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Unban", async () => {
+    const body = new FormData();
+    body.append("profileNumber", PROFILE);
+    body.append("banStatus", "0");
+
+    const res = await fetch(`${API}/setProfileBanStatus`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+});
+
+Deno.test("Edit profile", async () => {
+  const body = new FormData();
+  body.append("youtube", "/@NeKz");
+  body.append("twitch", "NeKzor");
+  body.append("boardname", "NeKz");
+
+  const res = await fetch(`${API}/editprofile`, {
+    method: "POST",
+    headers: {
+      "Cookie": COOKIE,
+    },
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  await res.body?.cancel();
+});
+
+Deno.test("Regenerate auth hash", async () => {
+  const res = await fetch(`${API}/regenerateAuthHash`, {
+    method: "POST",
+    headers: {
+      "Cookie": COOKIE,
+    },
+  });
+
+  assertEquals(res.status, 200);
+
+  await res.body?.cancel();
+});
+
+Deno.test("Fetch new user data", async () => {
+  const body = new FormData();
+  body.append("profileNumber", PROFILE);
+
+  const res = await fetch(`${API}/fetchNewUserData`, {
+    method: "POST",
+    headers: {
+      "Cookie": COOKIE,
+    },
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  await res.body?.cancel();
+});

tests/v2_test.ts
@@ -1,0 +1,169 @@
+import { assert, assertEquals } from "jsr:@std/assert";
+
+const API = "https://board.portal2.local";
+const PROFILE = "76561198049848090";
+const AUTH_HASH = "g2SxqP7Iv43Bi3uX4kEJeUUr2IWhf7jg";
+const COOKIE = "PHPSESSID=rfaktvssd448o1t93nvnhh187t";
+
+Deno.test("Validate user", async () => {
+  const body = new FormData();
+  body.append("auth_hash", AUTH_HASH);
+
+  const res = await fetch(`${API}/api-v2/validate-user`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  const json = await res.json();
+  assert(typeof json === "object");
+
+  assertEquals(json.userId, PROFILE);
+});
+
+Deno.test("Validate user error", async () => {
+  const body = new FormData();
+  body.append("auth_hash", "ASDF");
+
+  const res = await fetch(`${API}/api-v2/validate-user`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 400);
+  assertEquals(await res.text(), "User validation failed");
+});
+
+Deno.test("Active profiles", async () => {
+  const body = new FormData();
+  body.append("months", "3");
+
+  const res = await fetch(`${API}/api-v2/active-profiles`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  const json = await res.json();
+  assert(typeof json === "object");
+  assert(typeof json.profiles === "object");
+  assert(json.profiles.length > 0);
+});
+
+Deno.test("Automatic submission", async (t) => {
+  let id = "";
+
+  await t.step("Auto submit", async () => {
+    const body = new FormData();
+    body.append("auth_hash", AUTH_HASH);
+    body.append("mapId", "47458");
+    body.append("score", "2300");
+    body.append(
+      "demoFile",
+      new Blob([await Deno.readFile("./tests/data/short.dem")]),
+    );
+    body.append("comment", "test");
+
+    const res = await fetch(`${API}/api-v2/auto-submit`, {
+      method: "POST",
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    const run = await res.json();
+    assert(typeof run === "object");
+
+    assertEquals(run.player_name, "NeKz");
+    assertEquals(
+      run.avatar,
+      "https://avatars.steamstatic.com/9a86e6554aee395b3ac37d96a808335363eb79ff_full.jpg",
+    );
+    assertEquals(run.profile_number, "76561198049848090");
+    assertEquals(run.score, 2300);
+    assert(run.id);
+    assert(run.pre_rank);
+    assertEquals(run.post_rank, 5);
+    assertEquals(run.wr_gain, 0);
+    assert(run.time_gained);
+    assertEquals(run.hasDemo, 1);
+    assertEquals(run.youtubeID, null);
+    assertEquals(run.note, "test");
+    assertEquals(run.banned, 0);
+    assertEquals(run.submission, 2);
+    assertEquals(run.pending, 0);
+    assertEquals(run.autorender_id, null);
+    assert(run.previous_score);
+    assertEquals(run.chamberName, "Portal Gun");
+    assertEquals(run.chapterId, 7);
+    assertEquals(run.mapid, "47458");
+    assert(run.improvement >= 0);
+    assert(run.rank_improvement >= 0);
+    assertEquals(run.pre_points, null);
+    assertEquals(run.post_point, null);
+    assertEquals(run.point_improvement, null);
+
+    id = run.id.toString();
+  });
+
+  await t.step("Get demo", async () => {
+    const res = await fetch(`${API}/getDemo?id=${id}`);
+
+    assertEquals(res.status, 200);
+
+    const demo = await res.arrayBuffer();
+    assertEquals(new TextDecoder().decode(demo.slice(0, 8)), "HL2DEMO\0");
+  });
+
+  await t.step("Delete submission", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteSubmission`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+});
+
+Deno.test("Current PB", async () => {
+  const body = new FormData();
+  body.append("auth_hash", AUTH_HASH);
+  body.append("mapId", "62758");
+
+  const res = await fetch(`${API}/api-v2/current-pb`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  const pb = await res.json();
+  assert(typeof pb === "object");
+
+  assertEquals(pb.time_gained, "2014-03-22 10:36:59");
+  assertEquals(pb.profile_number, "76561198049848090");
+  assertEquals(pb.score, 3006);
+  assertEquals(pb.map_id, "62758");
+  assertEquals(pb.wr_gain, 0);
+  assertEquals(pb.has_demo, 0);
+  assertEquals(pb.banned, 0);
+  assertEquals(pb.youtube_id, null);
+  assertEquals(pb.previous_id, null);
+  assertEquals(pb.id, 33541);
+  assertEquals(pb.post_rank, 8);
+  assertEquals(pb.pre_rank, null);
+  assertEquals(pb.submission, 0);
+  assertEquals(pb.note, null);
+  assertEquals(pb.pending, 0);
+  assertEquals(pb.autorender_id, null);
+});

NeKzor/board

tests/auth_test.ts
@@ -1,0 +1,313 @@
+import { assert, assertEquals } from "jsr:@std/assert";
+
+const API = "https://board.portal2.local";
+const PROFILE = "76561198049848090";
+const COOKIE = "PHPSESSID=s3qqdj2g5d3ujpa0gdda2eb05p";
+
+Deno.test("Manual submission", async (t) => {
+  let id = "";
+
+  await t.step("Submit change", async () => {
+    const body = new FormData();
+    body.append("profileNumber", PROFILE);
+    body.append("chamber", "1");
+    body.append("score", "2300");
+    body.append("youtubeID", "DM3a55hXiI0");
+    body.append(
+      "demoFile",
+      new Blob([await Deno.readFile("./tests/data/short.dem")]),
+    );
+    body.append("comment", "test");
+
+    const res = await fetch(`${API}/submitChange`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    const run = await res.json();
+    assert(typeof run === "object");
+
+    assertEquals(run.player_name, "NeKz");
+    assertEquals(
+      run.avatar,
+      "https://avatars.steamstatic.com/9a86e6554aee395b3ac37d96a808335363eb79ff_full.jpg",
+    );
+    assertEquals(run.profile_number, "76561198049848090");
+    assertEquals(run.score, 2300);
+    assert(run.id);
+    assert(run.pre_rank);
+    assertEquals(run.post_rank, 5);
+    assertEquals(run.wr_gain, 0);
+    assert(run.time_gained);
+    assertEquals(run.hasDemo, 1);
+    assertEquals(run.youtubeID, "DM3a55hXiI0");
+    assertEquals(run.note, "test");
+    assertEquals(run.banned, 0);
+    assertEquals(run.submission, 1);
+    assertEquals(run.pending, 0);
+    assertEquals(run.autorender_id, null);
+    assert(run.previous_score);
+    assertEquals(run.chamberName, "Tramride");
+    assertEquals(run.chapterId, 7);
+    assertEquals(run.mapid, "1");
+    assert(run.improvement >= 0);
+    assert(run.rank_improvement >= 0);
+    assertEquals(run.pre_points, null);
+    assertEquals(run.post_point, null);
+    assertEquals(run.point_improvement, null);
+
+    id = run.id.toString();
+  });
+
+  await t.step("Delete comment", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteComment`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Set comment", async () => {
+    const body = new FormData();
+    body.append("id", id);
+    body.append("comment", "test 123");
+
+    const res = await fetch(`${API}/setComment`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Delete YouTube ID", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteYoutubeID`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Set YouTube ID", async () => {
+    const body = new FormData();
+    body.append("id", id);
+    body.append("youtubeID", "DM3a55hXiI0");
+
+    const res = await fetch(`${API}/setYoutubeID`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Delete demo", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteDemo`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Upload demo", async () => {
+    const body = new FormData();
+    body.append("id", id);
+    body.append(
+      "demoFile",
+      new Blob([await Deno.readFile("./tests/data/short.dem")]),
+    );
+
+    const res = await fetch(`${API}/uploadDemo`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Set score ban status", async () => {
+    const body = new FormData();
+    body.append("id", "82802");
+    body.append("banStatus", "1");
+
+    const res = await fetch(`${API}/setScoreBanStatus`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Verify score", async () => {
+    const body = new FormData();
+    body.append("id", "82802");
+
+    const res = await fetch(`${API}/verifyScore`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Delete submission", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteSubmission`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+});
+
+Deno.test("Set profile ban status", async (t) => {
+  await t.step("Ban", async () => {
+    const body = new FormData();
+    body.append("profileNumber", PROFILE);
+    body.append("banStatus", "1");
+
+    const res = await fetch(`${API}/setProfileBanStatus`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+
+  await t.step("Unban", async () => {
+    const body = new FormData();
+    body.append("profileNumber", PROFILE);
+    body.append("banStatus", "0");
+
+    const res = await fetch(`${API}/setProfileBanStatus`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+});
+
+Deno.test("Edit profile", async () => {
+  const body = new FormData();
+  body.append("youtube", "/@NeKz");
+  body.append("twitch", "NeKzor");
+  body.append("boardname", "NeKz");
+
+  const res = await fetch(`${API}/editprofile`, {
+    method: "POST",
+    headers: {
+      "Cookie": COOKIE,
+    },
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  await res.body?.cancel();
+});
+
+Deno.test("Regenerate auth hash", async () => {
+  const res = await fetch(`${API}/regenerateAuthHash`, {
+    method: "POST",
+    headers: {
+      "Cookie": COOKIE,
+    },
+  });
+
+  assertEquals(res.status, 200);
+
+  await res.body?.cancel();
+});
+
+Deno.test("Fetch new user data", async () => {
+  const body = new FormData();
+  body.append("profileNumber", PROFILE);
+
+  const res = await fetch(`${API}/fetchNewUserData`, {
+    method: "POST",
+    headers: {
+      "Cookie": COOKIE,
+    },
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  await res.body?.cancel();
+});

tests/v2_test.ts
@@ -1,0 +1,169 @@
+import { assert, assertEquals } from "jsr:@std/assert";
+
+const API = "https://board.portal2.local";
+const PROFILE = "76561198049848090";
+const AUTH_HASH = "yJMh2Q3QRa45Sc7vg9QeFjNLQTaq4u1N";
+const COOKIE = "PHPSESSID=s3qqdj2g5d3ujpa0gdda2eb05p";
+
+Deno.test("Validate user", async () => {
+  const body = new FormData();
+  body.append("auth_hash", AUTH_HASH);
+
+  const res = await fetch(`${API}/api-v2/validate-user`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  const json = await res.json();
+  assert(typeof json === "object");
+
+  assertEquals(json.userId, PROFILE);
+});
+
+Deno.test("Validate user error", async () => {
+  const body = new FormData();
+  body.append("auth_hash", "ASDF");
+
+  const res = await fetch(`${API}/api-v2/validate-user`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 400);
+  assertEquals(await res.text(), "User validation failed");
+});
+
+Deno.test("Active profiles", async () => {
+  const body = new FormData();
+  body.append("months", "3");
+
+  const res = await fetch(`${API}/api-v2/active-profiles`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  const json = await res.json();
+  assert(typeof json === "object");
+  assert(typeof json.profiles === "object");
+  assert(json.profiles.length > 0);
+});
+
+Deno.test("Automatic submission", async (t) => {
+  let id = "";
+
+  await t.step("Auto submit", async () => {
+    const body = new FormData();
+    body.append("auth_hash", AUTH_HASH);
+    body.append("mapId", "47458");
+    body.append("score", "2300");
+    body.append(
+      "demoFile",
+      new Blob([await Deno.readFile("./tests/data/short.dem")]),
+    );
+    body.append("comment", "test");
+
+    const res = await fetch(`${API}/api-v2/auto-submit`, {
+      method: "POST",
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    const run = await res.json();
+    assert(typeof run === "object");
+
+    assertEquals(run.player_name, "NeKz");
+    assertEquals(
+      run.avatar,
+      "https://avatars.steamstatic.com/9a86e6554aee395b3ac37d96a808335363eb79ff_full.jpg",
+    );
+    assertEquals(run.profile_number, "76561198049848090");
+    assertEquals(run.score, 2300);
+    assert(run.id);
+    assert(run.pre_rank);
+    assertEquals(run.post_rank, 5);
+    assertEquals(run.wr_gain, 0);
+    assert(run.time_gained);
+    assertEquals(run.hasDemo, 1);
+    assertEquals(run.youtubeID, null);
+    assertEquals(run.note, "test");
+    assertEquals(run.banned, 0);
+    assertEquals(run.submission, 2);
+    assertEquals(run.pending, 0);
+    assertEquals(run.autorender_id, null);
+    assert(run.previous_score);
+    assertEquals(run.chamberName, "Portal Gun");
+    assertEquals(run.chapterId, 7);
+    assertEquals(run.mapid, "47458");
+    assert(run.improvement >= 0);
+    assert(run.rank_improvement >= 0);
+    assertEquals(run.pre_points, null);
+    assertEquals(run.post_point, null);
+    assertEquals(run.point_improvement, null);
+
+    id = run.id.toString();
+  });
+
+  await t.step("Get demo", async () => {
+    const res = await fetch(`${API}/getDemo?id=${id}`);
+
+    assertEquals(res.status, 200);
+
+    const demo = await res.arrayBuffer();
+    assertEquals(new TextDecoder().decode(demo.slice(0, 8)), "HL2DEMO\0");
+  });
+
+  await t.step("Delete submission", async () => {
+    const body = new FormData();
+    body.append("id", id);
+
+    const res = await fetch(`${API}/deleteSubmission`, {
+      method: "POST",
+      headers: {
+        "Cookie": COOKIE,
+      },
+      body,
+    });
+
+    assertEquals(res.status, 200);
+
+    await res.body?.cancel();
+  });
+});
+
+Deno.test("Current PB", async () => {
+  const body = new FormData();
+  body.append("auth_hash", AUTH_HASH);
+  body.append("mapId", "1");
+
+  const res = await fetch(`${API}/api-v2/current-pb`, {
+    method: "POST",
+    body,
+  });
+
+  assertEquals(res.status, 200);
+
+  const pb = await res.json();
+  assert(typeof pb === "object");
+
+  assertEquals(pb.time_gained, "2024-06-02 20:46:45");
+  assertEquals(pb.profile_number, "76561198049848090");
+  assertEquals(pb.score, 6400);
+  assertEquals(pb.map_id, "1");
+  assertEquals(pb.wr_gain, 0);
+  assertEquals(pb.has_demo, 1);
+  assertEquals(pb.banned, 0);
+  assertEquals(pb.youtube_id, null);
+  assertEquals(pb.previous_id, null);
+  assertEquals(pb.id, 33541);
+  assertEquals(pb.post_rank, 8);
+  assertEquals(pb.pre_rank, null);
+  assertEquals(pb.submission, 0);
+  assertEquals(pb.note, null);
+  assertEquals(pb.pending, 0);
+  assertEquals(pb.autorender_id, null);
+});