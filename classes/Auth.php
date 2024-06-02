p2sr/Portal2Boards

Make all database queries safe This patch introduces the following changes for safety:
Database::unsafe_raw
Rename old query function to unsafe_raw. This should only be used when
dealing with queries that require no input parameters.

Database::query
The new query function takes in two additional parameters: types and
params. It's essentially just a wrapper around mysqli_stmt::bind_param.

Database::findOne
Same as Database::query but returns a single row. This function will
throw if the database returned more than one row.

Database::findMany
Same as Database::query but returns all rows.

For additional runtime type-safety all functions that make a database
requests will have a known type in the signature.

Furthermore, least portals code has been removed in order to reduce
the amount of code that has to be maintained.

classes/Auth.php
@@ -1,49 +1,62 @@
 <?php
 
 class Auth {
-
-    public static function gen_auth_hash($userId)
-    {
-        if (!ctype_alnum($userId)) return null;
-
+    public static function gen_auth_hash(string $profile_number) {
         // Create Auth hash
         $auth_hash = Util::random_str(32);
-        //Debug::log("User id: ".$userId." - Hash: ".$auth_hash);
+
         // Save to db
-        Database::query("UPDATE usersnew 
-                                SET usersnew.auth_hash = '{$auth_hash}'
-                                WHERE usersnew.profile_number = '{$userId}'");
-        // return auth hash
+        Database::query(
+            "UPDATE usersnew 
+             SET usersnew.auth_hash = ?
+             WHERE usersnew.profile_number = ?",
+            "ss",
+            [
+                $auth_hash,
+                $profile_number,
+            ]
+        );
+
         return $auth_hash;
     }
 
-    public static function test_auth_hash($auth_hash){
-        if (!ctype_alnum($auth_hash)) return null;
+    public static function test_auth_hash(string $auth_hash) {
+        $row = Database::findOne(
+            "SELECT usersnew.profile_number
+             FROM usersnew
+             WHERE usersnew.auth_hash = ?",
+            "s",
+            [
+                $auth_hash,
+            ]
+        );
 
-        $data = Database::query("SELECT usersnew.profile_number FROM usersnew
-                                WHERE usersnew.auth_hash = '{$auth_hash}'");
-
-        $userId = null;
-        while ($row = $data->fetch_assoc()) {
-            $userId = $row["profile_number"];
-        }
-        return $userId;
+        return $row ? strval($row["profile_number"]) : null;
     }
 
-    public static function get_auth_hash($userId) {
-        if (!ctype_alnum($userId)) return null;
-        $data = Database::query("SELECT usersnew.auth_hash FROM usersnew WHERE profile_number = {$userId}");
-        $auth_hash = null;
-        while ($row = $data->fetch_assoc()) {
-            $auth_hash = $row["auth_hash"];
-        }
-        return $auth_hash;
+    public static function get_auth_hash(string $profile_number) {
+        $row = Database::findOne(
+            "SELECT usersnew.auth_hash
+             FROM usersnew
+             WHERE profile_number = ?",
+            "s",
+            [
+                $profile_number,
+            ]
+        );
+
+        return $row ? strval($row["auth_hash"]) : null;
     }
 
-    public static function del_auth_hash($userId){
-        if (!ctype_alnum($userId)) return null;
-        Database::query("UPDATE usersnew 
-                                SET usersnew.auth_hash = NULL
-                                WHERE usersnew.profile_number = ".$userId);
+    public static function del_auth_hash(string $profile_number) {
+        Database::query(
+            "UPDATE usersnew 
+             SET usersnew.auth_hash = NULL
+             WHERE usersnew.profile_number = ?",
+            "s",
+            [
+                $profile_number,
+            ]
+        );
     }
 }

classes/DemoManager.php
@@ -7,29 +7,46 @@ class DemoManager {
         mkdir(ROOT_PATH . DemoManager::demoFolder);
     }
 
-    function getDemoName($id) {
-        $data = Database::query("SELECT changelog.profile_number, score, map_id
-              FROM changelog INNER JOIN usersnew ON (changelog.profile_number = usersnew.profile_number)
-              WHERE changelog.id = '" . $id . "'");
-        $row = $data->fetch_assoc();
+    function getDemoName(int $id) {
+        $row = Database::findOne(
+            "SELECT changelog.profile_number
+                  , score
+                  , map_id
+             FROM changelog
+             INNER JOIN usersnew ON changelog.profile_number = usersnew.profile_number
+             WHERE changelog.id = ?",
+            "i",
+            [
+                $id,
+            ]
+        );
+
         $map = str_replace(" ", "" , $GLOBALS["mapInfo"]["maps"][$row["map_id"]]["mapName"]);
         return $map."_".$row["score"]."_".$row["profile_number"]."_".$id.".dem";
     }
 
-    function getDemoDetails($id) {
-        $data = Database::query("SELECT changelog.id, changelog.profile_number, map_id
-              FROM changelog INNER JOIN usersnew ON (changelog.profile_number = usersnew.profile_number)
-              WHERE changelog.id = '" . $id . "'");
-        $row = $data->fetch_assoc();
+    function getDemoDetails(int $id) {
+        $row = Database::findOne(
+            "SELECT changelog.id
+                  , changelog.profile_number
+                  , map_id
+             FROM changelog
+             INNER JOIN usersnew ON changelog.profile_number = usersnew.profile_number
+             WHERE changelog.id = ?",
+            "i",
+            [
+                $id,
+            ]
+        );
+
         return $row;
     }
 
-
-    function getDemoPath($id) {
+    function getDemoPath(int $id) {
         return ROOT_PATH . '/' . DemoManager::demoFolder . '/' . $this->getDemoName($id);
     }
 
-    function getDemoURL($id) {
+    function getDemoURL(int $id) {
         $name = $this->getDemoName($id);
         $path = $this->getDemoPath($id);
         if (file_exists($path)) {
@@ -39,7 +56,7 @@ class DemoManager {
         }
     }
 
-    function uploadDemo($data, $id) {
+    function uploadDemo($data, int $id) {
         Debug::log("Uploading demo for changelog $id");
 
         $path = $this->getDemoPath($id);
@@ -55,7 +72,7 @@ class DemoManager {
         return $path;
     }
 
-    function deleteDemo($id) {
+    function deleteDemo(int $id) {
         Debug::log("Deleting demo for changelog $id");
         $path = $this->getDemoPath($id);
         if (!unlink($path)) {

util/fetchImportantProfileData.php
@@ -7,7 +7,11 @@
     Debug::initializeFileLogging();
     Debug::$loggingToOutput = true;
 
-    $data = Database::query("SELECT DISTINCT profile_number FROM changelog WHERE time_gained > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 DAY)");
+    $data = Database::unsafe_raw(
+        "SELECT DISTINCT profile_number
+         FROM changelog
+         WHERE time_gained > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 DAY)"
+    );
 
     $activeProfiles = array();
     while ($row = $data->fetch_assoc()) {
@@ -53,7 +57,11 @@
 
     print_r("Skillfull profiles: " . count($skillFullProfiles) . "\n");
 
-    $data = Database::query("SELECT profile_number FROM usersnew WHERE banned = 1");
+    $data = Database::unsafe_raw(
+        "SELECT profile_number
+         FROM usersnew
+         WHERE banned = 1"
+    );
     
     $bannedProfiles = array();
     while ($row = $data->fetch_assoc()) {

NeKzor/board

classes/Auth.php
@@ -1,49 +1,62 @@
 <?php
 
 class Auth {
-
-    public static function gen_auth_hash($userId)
-    {
-        if (!ctype_alnum($userId)) return null;
-
+    public static function gen_auth_hash(string $profile_number) {
         // Create Auth hash
         $auth_hash = Util::random_str(32);
-        //Debug::log("User id: ".$userId." - Hash: ".$auth_hash);
+
         // Save to db
-        Database::query("UPDATE users 
-                                SET users.auth_hash = '{$auth_hash}'
-                                WHERE users.profile_number = '{$userId}'");
-        // return auth hash
+        Database::query(
+            "UPDATE users 
+             SET users.auth_hash = ?
+             WHERE users.profile_number = ?",
+            "ss",
+            [
+                $auth_hash,
+                $profile_number,
+            ]
+        );
+
         return $auth_hash;
     }
 
-    public static function test_auth_hash($auth_hash){
-        if (!ctype_alnum($auth_hash)) return null;
+    public static function test_auth_hash(string $auth_hash) {
+        $row = Database::findOne(
+            "SELECT users.profile_number
+             FROM users
+             WHERE users.auth_hash = ?",
+            "s",
+            [
+                $auth_hash,
+            ]
+        );
 
-        $data = Database::query("SELECT users.profile_number FROM users
-                                WHERE users.auth_hash = '{$auth_hash}'");
-
-        $userId = null;
-        while ($row = $data->fetch_assoc()) {
-            $userId = $row["profile_number"];
-        }
-        return $userId;
+        return $row ? strval($row["profile_number"]) : null;
     }
 
-    public static function get_auth_hash($userId) {
-        if (!ctype_alnum($userId)) return null;
-        $data = Database::query("SELECT users.auth_hash FROM users WHERE profile_number = {$userId}");
-        $auth_hash = null;
-        while ($row = $data->fetch_assoc()) {
-            $auth_hash = $row["auth_hash"];
-        }
-        return $auth_hash;
+    public static function get_auth_hash(string $profile_number) {
+        $row = Database::findOne(
+            "SELECT users.auth_hash
+             FROM users
+             WHERE profile_number = ?",
+            "s",
+            [
+                $profile_number,
+            ]
+        );
+
+        return $row ? strval($row["auth_hash"]) : null;
     }
 
-    public static function del_auth_hash($userId){
-        if (!ctype_alnum($userId)) return null;
-        Database::query("UPDATE users 
-                                SET users.auth_hash = NULL
-                                WHERE users.profile_number = ".$userId);
+    public static function del_auth_hash(string $profile_number) {
+        Database::query(
+            "UPDATE users 
+             SET users.auth_hash = NULL
+             WHERE users.profile_number = ?",
+            "s",
+            [
+                $profile_number,
+            ]
+        );
     }
 }

classes/DemoManager.php
@@ -3,13 +3,22 @@
 class DemoManager {
     const demoFolder = ROOT_PATH . "/demos";
 
-    function getDemoName($id) {
-        $data = Database::query("SELECT changelog.profile_number, score, map_id, time_gained
-              FROM changelog INNER JOIN users ON (changelog.profile_number = users.profile_number)
-              WHERE changelog.id = '" . $id . "'");
-        $row = $data->fetch_assoc();
+    function getDemoName(int $id) {
+        $row = Database::findOne(
+            "SELECT changelog.profile_number
+                  , score
+                  , map_id
+             FROM changelog
+             INNER JOIN users ON changelog.profile_number = users.profile_number
+             WHERE changelog.id = ?",
+            "i",
+            [
+                $id,
+            ]
+        );
 
         $dir = (new DateTime($row["time_gained"]))->format('Y/m');
+
         $map = str_replace(" ", "" , $GLOBALS["mapInfo"]["maps"][$row["map_id"]]["mapName"]);
 
         return [
@@ -18,11 +27,20 @@ class DemoManager {
         ];
     }
 
-    function getDemoDetails($id) {
-        $data = Database::query("SELECT changelog.id, changelog.profile_number, map_id
-              FROM changelog INNER JOIN users ON (changelog.profile_number = users.profile_number)
-              WHERE changelog.id = '" . $id . "'");
-        $row = $data->fetch_assoc();
+    function getDemoDetails(int $id) {
+        $row = Database::findOne(
+            "SELECT changelog.id
+                  , changelog.profile_number
+                  , map_id
+             FROM changelog
+             INNER JOIN users ON changelog.profile_number = users.profile_number
+             WHERE changelog.id = ?",
+            "i",
+            [
+                $id,
+            ]
+        );
+
         return $row;
     }
 
@@ -37,7 +55,7 @@ class DemoManager {
         }
     }
 
-    function uploadDemo($data, $id) {
+    function uploadDemo($data, int $id) {
         Debug::log("Uploading demo for changelog $id");
 
         [$dir, $name] = $this->getDemoName($id);
@@ -59,7 +77,7 @@ class DemoManager {
         return $path;
     }
 
-    function deleteDemo($id) {
+    function deleteDemo(int $id) {
         Debug::log("Deleting demo for changelog $id");
 
         [$dir, $name] = $this->getDemoName($id);

util/fetchImportantProfileData.php
@@ -7,7 +7,11 @@
     Debug::initializeFileLogging();
     Debug::$loggingToOutput = true;
 
-    $data = Database::query("SELECT DISTINCT profile_number FROM changelog WHERE time_gained > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 DAY)");
+    $data = Database::unsafe_raw(
+        "SELECT DISTINCT profile_number
+         FROM changelog
+         WHERE time_gained > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 DAY)"
+    );
 
     $activeProfiles = array();
     while ($row = $data->fetch_assoc()) {
@@ -53,7 +57,11 @@
 
     print_r("Skillfull profiles: " . count($skillFullProfiles) . "\n");
 
-    $data = Database::query("SELECT profile_number FROM users WHERE banned = 1");
+    $data = Database::unsafe_raw(
+        "SELECT profile_number
+         FROM users
+         WHERE banned = 1"
+    );
     
     $bannedProfiles = array();
     while ($row = $data->fetch_assoc()) {