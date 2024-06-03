p2sr/Portal2Boards

Add a button to start a missing render

views/util/ChamberView.php
@@ -48,6 +48,8 @@ class ChamberView
             <div class="youtube">
                 <i <?php if ($scoreData["autorender_id"] !== NULL): ?>
                     onclick="window.open('https://autorender.portal2.sr/videos/<?=$scoreData["autorender_id"]?>','_blank')" class="youtubeEmbedButton fa fa-play" title="Auto Render"
+                <?php elseif (SteamSignIn::loggedInUserIsAdmin() && $scoreData["hasDemo"] === 1): ?>
+                    onclick="window.open('https://autorender.portal2.sr/render/portal2/<?=$scoreData["changelogId"]?>','_blank')" class="youtubeEmbedButton fa fa-video-camera" title="Start a render"
                 <?php else: ?>
                     style="display:none"
                 <?php endif; ?>

views/util/ChangelogView.php
@@ -89,6 +89,8 @@ class ChangelogView
             <div class="youtube">
                     <i <?php if ($val["autorender_id"] !== NULL): ?>
                             onclick="window.open('https://autorender.portal2.sr/videos/<?=$val["autorender_id"]?>','_blank')" class="youtubeEmbedButton fa fa-play" title="Auto Render"
+                        <?php elseif (SteamSignIn::loggedInUserIsAdmin() && $val["hasDemo"] === 1): ?>
+                            onclick="window.open('https://autorender.portal2.sr/render/portal2/<?=$val["id"]?>','_blank')" class="youtubeEmbedButton fa fa-video-camera" title="Start a render"
                         <?php else: ?>
                             style="display:none"
                         <?php endif; ?>

views/util/ProfileView.php
@@ -60,6 +60,8 @@ class ProfileView
                 <div class="cell youtube" align="center">
                     <i <?php if ($score["autorender_id"] !== NULL): ?>
                         onclick="window.open('https://autorender.portal2.sr/videos/<?=$score["autorender_id"]?>','_blank')" class="youtubeEmbedButton fa fa-play" title="Auto Render"
+                    <?php elseif (SteamSignIn::loggedInUserIsAdmin() && $score["hasDemo"] === 1): ?>
+                        onclick="window.open('https://autorender.portal2.sr/render/portal2/<?=$score["changelogId"]?>','_blank')" class="youtubeEmbedButton fa fa-video-camera" title="Start a render"
                     <?php else: ?>
                         style="display:none"
                     <?php endif; ?>

NeKzor/board

views/util/ChamberView.php
@@ -48,6 +48,8 @@ class ChamberView
             <div class="youtube">
                 <i <?php if ($scoreData["autorender_id"] !== NULL): ?>
                     onclick="window.open('https://autorender.portal2.sr/videos/<?=$scoreData["autorender_id"]?>','_blank')" class="youtubeEmbedButton fa fa-play" title="Auto Render"
+                <?php elseif (SteamSignIn::loggedInUserIsAdmin() && $scoreData["hasDemo"] === 1): ?>
+                    onclick="window.open('https://autorender.portal2.sr/render/mel/<?=$scoreData["changelogId"]?>','_blank')" class="youtubeEmbedButton fa fa-video-camera" title="Start a render"
                 <?php else: ?>
                     style="display:none"
                 <?php endif; ?>

views/util/ChangelogView.php
@@ -89,6 +89,8 @@ class ChangelogView
             <div class="youtube">
                     <i <?php if ($val["autorender_id"] !== NULL): ?>
                             onclick="window.open('https://autorender.portal2.sr/videos/<?=$val["autorender_id"]?>','_blank')" class="youtubeEmbedButton fa fa-play" title="Auto Render"
+                        <?php elseif (SteamSignIn::loggedInUserIsAdmin() && $val["hasDemo"] === 1): ?>
+                            onclick="window.open('https://autorender.portal2.sr/render/mel/<?=$val["id"]?>','_blank')" class="youtubeEmbedButton fa fa-video-camera" title="Start a render"
                         <?php else: ?>
                             style="display:none"
                         <?php endif; ?>

views/util/ProfileView.php
@@ -60,6 +60,8 @@ class ProfileView
                 <div class="cell youtube" align="center">
                     <i <?php if ($score["autorender_id"] !== NULL): ?>
                         onclick="window.open('https://autorender.portal2.sr/videos/<?=$score["autorender_id"]?>','_blank')" class="youtubeEmbedButton fa fa-play" title="Auto Render"
+                    <?php elseif (SteamSignIn::loggedInUserIsAdmin() && $score["hasDemo"] === 1): ?>
+                        onclick="window.open('https://autorender.portal2.sr/render/mel/<?=$score["changelogId"]?>','_blank')" class="youtubeEmbedButton fa fa-video-camera" title="Start a render"
                     <?php else: ?>
                         style="display:none"
                     <?php endif; ?>